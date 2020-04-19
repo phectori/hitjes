3a4283016155f154f1a13b9ec4d25e1786474b6e
Frits Kuipers <frits.kuipers@gmail.com>
2020-04-19 14:08:37 +0200

    Change into package

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hitjes-phectori", # Replace with your own username
+    version="0.0.1",
+    author="Frits Kuipers",
+    description="Listen to the same youtube list simultaneously",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/phectori/hitjes",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
