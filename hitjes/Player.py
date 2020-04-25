2190f5b6d86bc4b8c7a71d3375a4c0c9e9079105
Frits Kuipers <frits.kuipers@gmail.com>
2020-04-25 13:03:55 +0200

    Add seperate settings file

--- hitjes/Player.py
+++ hitjes/Player.py
@@ -4,65 +4,68 @@ import googleapiclient.discovery
 import random
 import os
 import logging
-import redis
-
+import threading
 from Error import YoutubeIdMalformedError
 
 
 class Player:
-    def __init__(self, _config, _broadcast_state, _broadcast_message):
-        self.r = redis.Redis(
-            host=_config["redis"].get("host", "localhost"),
-            port=int(_config["redis"].get("Port", 6379)),
-            db=0,
-        )
-
-        # Register callbacks
-        self.cb_broadcast_state = _broadcast_state
-        self.cb_broadcast_message = _broadcast_message
-
+    def __init__(self, _broadcast_state, _broadcast_message):
         self.queue = deque([])
         self.history = deque([])
+        self.cb_broadcast_state = _broadcast_state
+        self.cb_broadcast_message = _broadcast_message
+        self.titles = defaultdict(lambda: '')
 
-        self.titles = defaultdict(lambda: "")
-        self.googleapikey = str(_config["googleapi"].get("DeveloperKey", ""))
+        # Semaphore for next function
+        self.sem = threading.Semaphore()
 
-        self.r.set('current_video_id', "")
-        self.r.set('current_video_title', "")
+        self.current = dict()
+        self.current["id"] = ""
+        self.current["title"] = ""
+        self.timestamp_s = 0
 
     def get_current(self) -> str:
-        return str(self.r.get('current_video_id'))
+        return self.current["id"]
 
+    def get_current_title(self) -> str:
+        return self.current["title"]
+
+    def get_timestamp(self):
+        return self.timestamp_s
 
     def update_timestamp(self, _timestamp_s, yt_id):
         """ Always take the highest timestamp of all the clients """
-        if self.r.get('current_video_id') != yt_id:
+        if self.current["id"] != yt_id:
             return
 
-        # if self.timestamp_s > _timestamp_s:
-        #     self.timestamp_s = _timestamp_s
+        if self.timestamp_s > _timestamp_s:
+            self.timestamp_s = _timestamp_s
 
     def next(self, current_id: str):
+        self.sem.acquire()
+
         # Only allow next when playing the current id.
-        if self.r.get('current_video_id') != current_id:
+        if self.current["id"] != current_id:
             logging.info("next: player video id does not match server video id")
             return
 
         # Add to history when not empty
-        if self.r.get('current_video_id') != "":
-            self.history.appendleft(self.r.get('current_video_id'))
+        if self.current["id"] != "":
+            self.history.appendleft(self.current)
 
         if len(self.queue) is not 0:
-            self.r.set('current_video_id', self.queue.popleft())
+            self.current = self.queue.popleft()
         else:
             logging.info("Queue empty")
             if len(self.history) > 0:
-                self.r.set('current_video_id', random.choice(list(self.history)))
+                self.current = random.choice(list(self.history))
                 self.cb_broadcast_message(
                     "Queue empty, selecting a random video from history."
                 )
             else:
-                self.r.set('current_video_id', "")
+                self.current["id"] = ""
+
+        self.sem.release()
 
         self.cb_broadcast_state()
 
@@ -71,28 +74,30 @@ class Player:
             raise YoutubeIdMalformedError(yt_id, "Received ID has the wrong length")
 
         logging.info("append: " + yt_id)
+        title = ""
         try:
-            self.get_video_title(yt_id)
+            title = self.get_video_title(yt_id)
+            logging.info("Got title: '{}'".format(title))
         except:
-            logging.error("Failed to get video title")
+            logging.error('Failed to get video title')
 
-        self.queue.append({"id": yt_id, "title": self.titles[yt_id]})
+        self.queue.append({"id": yt_id, "title": title})
 
-        if self.r.get('current_video_id') == "":
+        self.cb_broadcast_message("Added: {}".format(title))
+
+        if self.current["id"] == "":
             # When tot playing anything
             self.next("")
         else:
             self.cb_broadcast_state()
 
     def get_queue(self) -> []:
-        logging.info("get_queue")
         return list(self.queue)
 
     def get_history(self) -> []:
-        logging.info("get_history")
         return list(self.history)
 
-    def get_video_title(self, yt_id: str):
+    def get_video_title(self, yt_id: str) -> str:
         logging.info("get_video_title")
         if yt_id in self.titles:
             return self.titles[yt_id]
@@ -112,6 +117,13 @@ class Player:
             cache_discovery=False,
         )
 
-        request = youtube.videos().list(part="snippet", id=yt_id)
+        request = youtube.videos().list(
+            part="snippet", id=yt_id
+        )
         response = request.execute()
-        self.titles[yt_id] = response["items"][0]["snippet"]["title"]
+        title = response["items"][0]["snippet"]["title"]
+
+        if len(title) > 0:
+            self.titles[yt_id] = title
+
+        return title
